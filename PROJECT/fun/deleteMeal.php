71de60c4bb74e21230526e8165215388bdd9d1d9
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2023-12-28 10:25:13 +0300

    Delete PROJECT directory

--- PROJECT/fun/deleteMeal.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("../connect.php");
-$d = htmlspecialchars($_GET["d"]);
-$sql = "DELETE FROM meal WHERE idMeal = $d";
-$result = $con->query($sql);
-echo json_encode($result);
-?>
--- PROJECT/fun/getAllMeal.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connect.php");
-$sql = "SELECT * FROM meal ";
-$result = $con->query($sql);
-while($row = $result->fetch_assoc()) {
-    $data[]=$row;
-}
-echo '{"meals":'. json_encode($data).'}';
-?>
